7ce78c14d1df973d7abe3d5443b11208c0ce629a
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2024-09-26 10:12:41 +0100

    Add MenuItemViewModel

---
+++ Mobile/Mobile/ItemViewModels/MenuItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Mobile.Models;
+using Mobile.Views;
+using Prism.Commands;
+using Prism.Navigation;
+
+namespace Mobile.ItemViewModels
+{
+	public class MenuItemViewModel : Menu
+	{
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectMenuCommand;
+
+        public DelegateCommand SelectMenuCommand => _selectMenuCommand ?? (_selectMenuCommand = new DelegateCommand(SelectMenuAsync));
+
+        public MenuItemViewModel(INavigationService navigationService)
+		{
+            _navigationService = navigationService;
+        }
+
+        private async void SelectMenuAsync()
+        {
+            await _navigationService.NavigateAsync($"/{nameof(MobileMasterDetailPage)}/NavigationPage/{PageName}");
+        }
+    }
+}
+
